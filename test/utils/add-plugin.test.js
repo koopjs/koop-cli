9a13a264667598603a6c2172065f120efe0310db
Haoliang Yu <hyu@esri.com>
2019-04-14 11:33:01 -0400

    add more tests

--- test/utils/add-plugin.test.js
+++ test/utils/add-plugin.test.js
@@ -5,6 +5,7 @@ const path = require('path')
 const chai = require('chai')
 const fs = require('fs-extra')
 const proxyquire = require('proxyquire')
+const createNewProject = require('./add-plugin/create-new-project')
 
 const modulePath = '../../src/utils/add-plugin'
 const addNpmPluginModulePath = path.join(modulePath, 'add-npm-plugin')
@@ -27,82 +28,6 @@ describe('utils/add-plugin', function () {
     appName = `add-command-test-${Date.now()}`
     appPath = path.join(temp, appName)
     await createNewProject(temp, 'app', appName, defaultOptions)
-  })
-
-  it('should add a plugin to an app project', async () => {
-    const addNpmPlugin = proxyquire(addNpmPluginModulePath, {
-      'latest-version': async () => '1.0.0'
-    })
-    const addPlugin = proxyquire(modulePath, {
-      './add-npm-plugin': addNpmPlugin
-    })
-    await addPlugin(appPath, 'provider', 'test-provider', defaultOptions)
-
-    const plugins = await fs.readFile(path.join(appPath, 'src', 'plugins.js'), 'utf-8')
-    const expected = [
-      "const testProvider = require('test-provider');",
-      'const outputs = [];',
-      'const auths = [];',
-      'const plugins = [{',
-      '  instance: testProvider',
-      '}];',
-      'module.exports = [...outputs, ...auths, ...plugins];'
-    ].join(os.EOL)
-    expect(plugins).to.equal(expected)
-
-    const packageInfo = await fs.readJson(path.join(appPath, 'package.json'))
-    expect(packageInfo.dependencies['test-provider']).to.equal('^1.0.0')
-  })
-
-  it('should add a plugin published as a scoped module', async () => {
-    const addNpmPlugin = proxyquire(addNpmPluginModulePath, {
-      'latest-version': async () => '1.0.0'
-    })
-    const addPlugin = proxyquire(modulePath, {
-      './add-npm-plugin': addNpmPlugin
-    })
-
-    await addPlugin(appPath, 'provider', '@koop/test-provider', defaultOptions)
-
-    const plugins = await fs.readFile(path.join(appPath, 'src', 'plugins.js'), 'utf-8')
-    const expected = [
-      "const testProvider = require('@koop/test-provider');",
-      'const outputs = [];',
-      'const auths = [];',
-      'const plugins = [{',
-      '  instance: testProvider',
-      '}];',
-      'module.exports = [...outputs, ...auths, ...plugins];'
-    ].join(os.EOL)
-    expect(plugins).to.equal(expected)
-
-    const packageInfo = await fs.readJson(path.join(appPath, 'package.json'))
-    expect(packageInfo.dependencies['@koop/test-provider']).to.equal('^1.0.0')
-  })
-
-  it('should add a plugin published with a version number', async () => {
-    const addNpmPlugin = proxyquire(addNpmPluginModulePath, {
-      'latest-version': async () => '3.2.1'
-    })
-    const addPlugin = proxyquire(modulePath, {
-      './add-npm-plugin': addNpmPlugin
-    })
-    await addPlugin(appPath, 'provider', '@koop/test-provider@3.2.0', defaultOptions)
-
-    const plugins = await fs.readFile(path.join(appPath, 'src', 'plugins.js'), 'utf-8')
-    const expected = [
-      "const testProvider = require('@koop/test-provider');",
-      'const outputs = [];',
-      'const auths = [];',
-      'const plugins = [{',
-      '  instance: testProvider',
-      '}];',
-      'module.exports = [...outputs, ...auths, ...plugins];'
-    ].join(os.EOL)
-    expect(plugins).to.equal(expected)
-
-    const packageInfo = await fs.readJson(path.join(appPath, 'package.json'))
-    expect(packageInfo.dependencies['@koop/test-provider']).to.equal('^3.2.1')
   })
 
   it('should add plugin config if provided', async () => {
@@ -228,13 +153,3 @@ describe('utils/add-plugin', function () {
     expect(plugins).to.equal(expected)
   })
 })
-
-async function createNewProject (cwd, type, name) {
-  const templatePath = path.resolve(__dirname, '../../src/templates/app/project')
-  const projectPath = path.join(cwd, name)
-
-  // just copy the app template and no need to use the formal project creator
-  // (skip some file I/O)
-  await fs.ensureDir(projectPath)
-  await fs.copy(templatePath, projectPath)
-}
