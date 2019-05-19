5ec92f5a04670ad2c94cba0e8d1874a78291e672
Haoliang Yu <hyu@esri.com>
2019-05-19 14:46:52 -0400

    update dependencies

---
+++ scripts/ncu.js
@@ -0,0 +1,19 @@
+const ncu = require('npm-check-updates')
+const packageFiles = [
+  './package.json',
+  './src/template/project/package.json',
+  './src/template/components/app/package.json',
+  './src/template/components/auth/package.json',
+  './src/template/components/provider/package.json'
+]
+
+async function upgrade () {
+  for (const file of packageFiles) {
+    await ncu.run({
+      packageFile: file,
+      upgrade: true
+    })
+  }
+}
+
+upgrade()
